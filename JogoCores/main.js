9bdb7fed9bf5da3bd3a8b76929e1826de436c79b
Alex José <saude.alex2010@gmail.com>
2023-09-03 16:42:04 -0300

    tres jogos

---
+++ JogoCores/main.js
@@ -0,0 +1,34 @@
+
+function rosa(){
+   document.body.style.backgroundColor = 'pink';
+}
+function azul(){
+    document.body.style.backgroundColor = 'blue';
+}
+function vermelha(){
+    document.body.style.backgroundColor = 'red';
+}
+function verde(){
+    document.body.style.backgroundColor = 'green';
+}
+function cinza(){
+    document.body.style.backgroundColor = 'gray';
+}
+function preta(){
+    document.body.style.backgroundColor = 'black';
+}
+function roxa(){
+    document.body.style.backgroundColor = 'purple';
+}
+function marrom(){
+    document.body.style.backgroundColor = 'brown';
+}
+function amarela(){
+    document.body.style.backgroundColor = 'yellow';
+}
+function laranja(){
+    document.body.style.backgroundColor = 'orange';
+}
+function branca(){
+    document.body.style.backgroundColor = 'white';
+}
